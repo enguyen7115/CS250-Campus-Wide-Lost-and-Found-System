05ff45ac22d5e367e0086013991923d4bc265252
AidanCram <acram4848@sdsu.edu>
2026-03-21 21:12:02 -0700

    Code added to Dashbaord.jsx and Dashboard.css created and edited

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
+import './Dashboard.css'
 export default function Dashboard() {
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>User reports and messages will appear here.</p>
+            <h1 className="header-box">Dashboard</h1>
+            <a className="color-box box-one" href="/Search">Search Item</a>
+            <a className="color-box box-two" href="/Report">Report Item</a>
+            <a className="color-box box-three" href="https://library2.sdsu.edu/wayfinder/">SDSU Map</a>
+            <a className="color-box box-four" href="/Chat">Chat System</a>
         </div>
     );
 }
